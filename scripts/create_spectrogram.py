b3fcc795f20e0010e935330a9f56ef959b6f4c1d
corrooli <corrooli@students.zhaw.ch>
2021-09-14 19:44:19 +0200

    Add our custom helper scripts.
    Co-Authored-By: fuerbringer <severin@fuerbringer.info>

---
+++ scripts/create_spectrogram.py
@@ -0,0 +1,24 @@
+
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+waveform, sample_rate = librosa.load('/home/oliver/Downloads/song.wav')
+
+def to_decibles(signal):
+    # Perform short time Fourier Transformation of signal and take absolute value of results
+    stft = np.abs(librosa.stft(signal))
+    # Convert to dB
+    D = librosa.amplitude_to_db(stft, ref = np.max) # Set reference value to the maximum value of stft.
+    return D # Return converted audio signal
+
+# Function to plot the converted audio signal
+def plot_spec(D, sr, instrument):
+    fig, ax = plt.subplots(figsize = (30,10))
+    spec = librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='linear', ax=ax)
+    ax.set(title = f'Sample rate: {sample_rate}')
+    fig.colorbar(spec)
+
+plot_spec(to_decibles(waveform), sample_rate, 'Guitar')
+plt.show()
