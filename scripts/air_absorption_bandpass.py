61093a9bec9930281abfb6fa2dfebb8633affedf
corrooli <corrooli@students.zhaw.ch>
2021-10-04 16:57:38 +0200

    Further optimizations
    Co-Authored-By: fuerbsev <fuerbsev@students.zhaw.ch>

--- scripts/air_absorption_bandpass.py
+++ scripts/air_absorption_bandpass.py
@@ -7,7 +7,6 @@ from multiprocessing import Pool
 
 
 class Bandpass(FilterStrategy):
-
     def __init__(self, max_frequency=20000, min_frequency=1, divisions=50, fs=44100):
         self.max_frequency = max_frequency
         self.min_frequency = min_frequency
@@ -18,16 +17,16 @@ class Bandpass(FilterStrategy):
     '''
     Calculates how much distance the sound has travelled. [m]
     '''
-
-    def distance_travelled(self, sample_number, sampling_frequency, c):
-        seconds_passed = sample_number*(sampling_frequency**(-1))
-        return (seconds_passed*c)  # [m]
+    @staticmethod
+    def distance_travelled(sample_number, sampling_frequency, c):
+        seconds_passed = sample_number * (sampling_frequency ** (-1))
+        return (seconds_passed * c)  # [m]
 
     '''
     Returns a butterworth bandpass filter.
     '''
-
-    def create_bandpass_filter(self, lowcut, highcut, fs, order=4):
+    @staticmethod
+    def create_bandpass_filter(lowcut, highcut, fs, order=10):
         nyq = 0.5 * fs
         low = (lowcut / nyq)
         high = highcut / nyq
@@ -37,31 +36,33 @@ class Bandpass(FilterStrategy):
     '''
     Applies a butterworth bandpass filter.
     '''
-
-    def apply_bandpass_filter(self, data, lowcut, highcut, fs, order=3):
-        b, a = self.create_bandpass_filter(lowcut, highcut, fs, order=order)
+    @staticmethod
+    def apply_bandpass_filter(data, lowcut, highcut, fs, order=10):
+        b, a = Bandpass.create_bandpass_filter(lowcut, highcut, fs, order=order)
         y = lfilter(b, a, data)
         return y
 
     def apply_single_band(self, IR, band_num, frequency_range):
+        band = frequency_range / self.divisions
+
         # Upper ceiling of each band
-        band_max = ((frequency_range / self.divisions) * band_num)
+        band_max = (band * band_num)
 
         # Lower ceiling of each band and handling of edge case
         if band_num == 1:
             band_min = self.min_frequency
         else:
-            band_min = ((frequency_range / self.divisions) * (band_num - 1))
+            band_min = (band * (band_num - 1))
 
         # Calculating mean frequency of band which determines the attenuation.
-        band_mean = (band_max+band_min)/2
+        band_mean = (band_max + band_min) / 2
 
         # Prepare + apply bandpass filter
         filtered_signal = self.apply_bandpass_filter(
             IR, band_min, band_max, self.fs, 3)
 
         # Calculate air absorption coefficients
-        alpha, alpha_iso, c, c_iso = aa.air_absorption(band_mean)
+        alpha, _, c, _ = aa.air_absorption(band_mean)
 
         # Apply attenuation
         for k in range(0, len(filtered_signal)):
@@ -71,13 +72,13 @@ class Bandpass(FilterStrategy):
 
         return filtered_signal
 
-    # max_frequency, min_frequency, divisions, fs
-
+    '''
+    Creates a multi processing pool and calls methods to apply bandpass based air absorption.
+    '''
     def air_absorption_bandpass(self, IR):
         pool = multiprocessing.Pool()
 
         frequency_range = self.max_frequency - self.min_frequency
-        combined_signals = np.zeros(len(IR))
 
         # Divide frequency range into defined frequency bands
         filtered_signals = pool.starmap(self.apply_single_band,
